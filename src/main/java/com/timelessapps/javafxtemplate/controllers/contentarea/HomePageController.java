118bf63e1c77888f43237947244e583b72b17a2d
MT9304 <MT9304@gmail.com>
2018-05-13 21:21:47 -0700

    Added logging formatting.

--- src/main/java/com/timelessapps/javafxtemplate/controllers/contentarea/HomePageController.java
+++ src/main/java/com/timelessapps/javafxtemplate/controllers/contentarea/HomePageController.java
@@ -1,19 +1,25 @@
 package main.java.com.timelessapps.javafxtemplate.controllers.contentarea;
 
+import java.awt.AWTException;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import main.java.com.timelessapps.javafxtemplate.app.businesslogic.MainBotRoutine;
 import main.java.com.timelessapps.javafxtemplate.app.supportingthreads.GlobalKeyListener;
+import main.java.com.timelessapps.javafxtemplate.helpers.services.LoggingService;
 import main.java.com.timelessapps.javafxtemplate.helpers.services.RobotService;
 
 public class HomePageController implements Initializable
 {
+    LoggingService log = new LoggingService();
     RobotService bot;
-    private boolean started = false;
+    private static boolean hasStarted = false;
     
     public HomePageController()
     {
@@ -23,11 +29,19 @@ public class HomePageController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        
+								if (!hasStarted)
+								{
+												try 
+												{
+																log.appendToEventLogsFile("ApplicationStarted. ");
+																log.appendToApplicationLogsFile("ApplicationStarted. ");
+												} catch (FileNotFoundException ex) {Logger.getLogger(HomePageController.class.getName()).log(Level.SEVERE, null, ex);}
+								}
+								hasStarted = true;
     }    
     
     @FXML
-    public void startApplication(MouseEvent event) throws InterruptedException 
+    public void startApplication(MouseEvent event) throws InterruptedException, AWTException 
     {
 	MainBotRoutine mainBotRoutine = new MainBotRoutine();
 	mainBotRoutine.setDaemon(true);
